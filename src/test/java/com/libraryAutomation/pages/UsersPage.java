e8a6493f7582ab7286a119593af02153ed43f74f
myizci <izci@hotmail.com>
2021-02-22 07:53:34 -0500

    US4 done and branch merged to remote

--- src/test/java/com/libraryAutomation/pages/UsersPage.java
+++ src/test/java/com/libraryAutomation/pages/UsersPage.java
@@ -1,4 +1,89 @@
 package com.libraryAutomation.pages;
 
+import com.github.javafaker.Faker;
+import com.libraryAutomation.utilities.BrowserUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class UsersPage extends BasePage{
+
+    Faker faker = new Faker();
+
+
+    @FindBy(xpath = "//a[@href='#users']")
+    private WebElement usersPageLink;
+
+    @FindBy(xpath = "//a[@href='tpl/add-user.html']")
+    private WebElement addUserLink;
+
+    @FindBy(xpath = "//input[@name='full_name']")
+    private WebElement fullNameBox;
+
+    @FindBy(xpath = "//input[@name='password']")
+    private WebElement passwordBox;
+
+    @FindBy(xpath = "//input[@name='email']")
+    private WebElement emailBox;
+
+    @FindBy(xpath = "//input[@name='start_date']")
+    private WebElement startDateBox;
+
+    @FindBy(xpath = "//input[@name='end_date']")
+    private WebElement endDateBox;
+
+    @FindBy(xpath = "//select[@id='user_group_id']")
+    private WebElement userGroupDropDown;
+
+    @FindBy(id = "status")
+    private WebElement statusDropDown;
+
+    @FindBy(id = "address")
+    private WebElement addressBox;
+
+    @FindBy(xpath = "//button[.='Save changes']")
+    private WebElement saveChangesButton;
+
+    @FindBy(xpath = "(//h5)[1]")
+    public WebElement editUserInfoTitle;
+
+    @FindBy(xpath = "//button[.='Close']")
+    public WebElement closeButton;
+
+    @FindBy(xpath = "(//table//td[1])[1]")
+    public WebElement editFirstUser;
+
+    @FindBy(xpath = "//h3")
+    public WebElement userManagementTitle;
+
+    private String name = faker.name().fullName();
+
+    public void navigateToUsersPage (){
+        usersPageLink.click();
+    }
+
+    public void addUser(){
+
+
+        LocalDate today = LocalDate.now();
+        LocalDate futureDate = today.plusYears(2);
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        addUserLink.click();
+        Select selectUserGroup = new Select(userGroupDropDown);
+        Select selectStatus = new Select(statusDropDown);
+        BrowserUtils.waitForVisibility(fullNameBox, 5);
+        fullNameBox.sendKeys(name);
+        passwordBox.sendKeys(faker.internet().password());
+        emailBox.sendKeys(faker.internet().emailAddress());
+        selectUserGroup.selectByVisibleText("Students");
+        selectStatus.selectByVisibleText("ACTIVE");
+        endDateBox.sendKeys(futureDate.format(df));
+        addressBox.sendKeys(faker.address().fullAddress());
+        saveChangesButton.click();
+    }
+
+
 }
